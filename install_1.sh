97914cec335a8353a9fd9a8901d24123ae6f8166
Stephen ELIE <stelie@student.42.fr>
2023-09-27 16:29:35 +0200

    added sleeps for debug

--- install_1.sh
+++ install_1.sh
@@ -8,12 +8,18 @@ setup-timezone -z UTC
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/main" > /etc/apk/repositories
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/community" >> /etc/apk/repositories
 
+sleep 5
+
 yes | setup-disk -q -m sys /dev/sda
 setup-sshd -c openssh
 setup-ntp -c chrony
+sleep 5
 apk update -y
 apk add -y sudo nano docker ufw
+sleep 5
 rc-update add docker default
 rc-update add ufw default
+sleep 5
 adduser -G sudo krozis
+sleep 5
 poweroff
