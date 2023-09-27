1215a65026fedee81cd9415243943609724e4179
Stephen ELIE <stelie@student.42.fr>
2023-09-27 16:10:27 +0200

    test

---
+++ install_1.sh
@@ -0,0 +1,10 @@
+setup-hostname -n alpinception
+setup-keymap us us-altgr-intl
+setup-timezone -z UTC
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/main" > /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/community" >> /etc/apk/repositories
+yes | setup-disk -q -m sys /dev/sda
+setup-sshd -c openssh
+setup-ntp -c chrony
+passwd
+poweroff
