1a0c0959dc6076d02b5b94a8cb25680a97c8b9e9
Stephen ELIE <stelie@student.42.fr>
2023-09-27 16:47:33 +0200

    test

---
+++ test.sh
@@ -0,0 +1,4 @@
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/main" > /etc/apk/repositories
+echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/community" >> /etc/apk/repositories
+apk update
+apk add sudo nano docker ufw
