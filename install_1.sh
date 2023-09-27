acd1a340b0d44d44f180df38fd50899a77fc2c23
Stephen ELIE <stelie@student.42.fr>
2023-09-27 16:31:51 +0200

    corrected apk add and removed 2 sleeps

--- install_1.sh
+++ install_1.sh
@@ -8,14 +8,11 @@ setup-timezone -z UTC
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/main" > /etc/apk/repositories
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/community" >> /etc/apk/repositories
 
-sleep 5
-
 yes | setup-disk -q -m sys /dev/sda
 setup-sshd -c openssh
 setup-ntp -c chrony
-sleep 5
-apk update -y
-apk add -y sudo nano docker ufw
+apk update
+apk add sudo nano docker ufw
 sleep 5
 rc-update add docker default
 rc-update add ufw default
