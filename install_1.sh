94dee5d04968d547d9e71b60420b9cd2b091c0a4
Stephen ELIE <stelie@student.42.fr>
2023-09-27 16:24:31 +0200

    added features

--- install_1.sh
+++ install_1.sh
@@ -1,10 +1,19 @@
+#!/bin/sh
+
+passwd
 setup-hostname -n alpinception
 setup-keymap us us-altgr-intl
 setup-timezone -z UTC
+
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/main" > /etc/apk/repositories
 echo "http://dl-cdn.alpinelinux.org/alpine/v3.18/community" >> /etc/apk/repositories
+
 yes | setup-disk -q -m sys /dev/sda
 setup-sshd -c openssh
 setup-ntp -c chrony
-passwd
+apk update -y
+apk add -y sudo nano docker ufw
+rc-update add docker default
+rc-update add ufw default
+adduser -G sudo krozis
 poweroff
