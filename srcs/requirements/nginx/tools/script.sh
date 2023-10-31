011699e9fd071e203be8dac98685fcb28f05c1f1
stelie <stelie@student.42.fr>
2023-10-31 15:00:40 -0400

    working nginx

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 openssl req -x509 -days 365 -newkey rsa:2048 -nodes \
-    -keyout $NGINX_SELFSIGNED_KEY \
-    -out $NGINX_CERTIFICATE \
+    -out /etc/ssl/certs/nginx_selfsigned.crt \
+    -keyout /etc/ssl/private/nginx_selfsigned.key \
     -subj "/C=FR/ST=Paris/O=42Born2Code/CN=stelie.42.fr"
-
-
