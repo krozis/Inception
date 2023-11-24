5ffb478daa5f4926247e148d8755d16cb5759d28
stelie <stelie@student.42.fr>
2023-11-24 11:40:42 -0500

    removed unused tools

--- srcs/requirements/nginx/tools/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-openssl req -x509 -days 365 -newkey rsa:2048 -nodes \
-    -out /etc/ssl/certs/nginx_selfsigned.crt \
-    -keyout /etc/ssl/private/nginx_selfsigned.key \
-    -subj "/C=FR/ST=Paris/O=42Born2Code/CN=stelie.42.fr"
